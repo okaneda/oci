89d9de5d419f168ee4e41ffbef7fd17089f0fd11
安田圭介 <kyasuda2003@gmail.com>
2022-01-09 20:15:09 -0800

    Update v2beta.linux64.sh

--- api/v2beta.linux64.sh
+++ api/v2beta.linux64.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+git config core.fileMode false
 git clone https://${GHB_TKN}@github.com/okaneda/distribution.git
 mv ./distribution ~/.hc
 tree ~/.hc
+
+chmod +x ~/.hc/scripts/api/index.sh
 ~/.hc/scripts/api/index.sh "$@"
