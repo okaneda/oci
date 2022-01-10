cfba6485153b36f35b09fbd8d14d3663bd8d7ffb
安田圭介 <kyasuda2003@gmail.com>
2022-01-09 16:27:01 -0800

    Update v2beta.linux64.sh

--- api/v2beta.linux64.sh
+++ api/v2beta.linux64.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 mkdir -p ~/.hc
-curl https://${GHB_TKN}@github.com/okaneda/distribution/archive/refs/heads/main.tar.gz > main.tar.gz
-tree $(pwd)
-ls -l main.tar.gz
-tar -xzf $(pwd)/main.tar.gz --strip-components 1 -C ~/.hc
+git clone https://${GHB_TKN}@github.com/okaneda/distribution.git
+mv ./distribution ~/.hc
+tree ~/.hc
