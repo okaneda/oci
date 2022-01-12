d2e3f86c304e5841b8851ef5a6aad59c57849473
安田圭介 <kyasuda2003@gmail.com>
2022-01-11 23:13:27 -0800

    Update and rename api/v2beta.linux64.sh to cell/v2beta.linux64.sh

---
+++ cell/v2beta.linux64.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(pwd)/.hc/scripts
+
+git clone --depth 1 https://${GHB_TKN}@github.com/okaneda/distribution.git
+mv ./distribution ~/.hc
+tree ~/.hc
+
+case $APP_MOD in
+  api)
+    echo "launch cell as api app"
+    chmod +x ${SCRIPT_PATH}/api/index.sh
+    source ${SCRIPT_PATH}/api/index.sh "$@"
+    ;;
+  oa2)
+    echo "launch cell as oauth2 app"
+    chmod +x ${SCRIPT_PATH}/oa2/index.sh
+    source ${SCRIPT_PATH}/oa2/index.sh "$@"
+    ;;
+  *)
+    echo "unavailable option (${APP_MOD})"
+    ;;
+esac
