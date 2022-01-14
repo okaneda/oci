2a4e6ef4e37e8fd165d536dcacf058f27dc5576f
安田圭介 <kyasuda2003@gmail.com>
2022-01-13 21:00:25 -0800

    Update v2-b.linux64.sh

--- app/v2-b.linux64.sh
+++ app/v2-b.linux64.sh
@@ -11,9 +11,13 @@ git clone --depth 1 https://${GH_TKN}@github.com/okaneda/distribution.git
 mv ./distribution $PATH_MAIN
 tree $PATH_MAIN
 
-printf "\ninstalling cell\n\n"
-chmod +x ${PATH_SCRIPT}/cell/v2-b.linux64.sh
-source ${PATH_SCRIPT}/cell/v2-b.linux64.sh "$@"
+{
+    ${CELL_BINARY} --ver
+} || {
+    printf "\nmissing cell bin. begining cell installation\n\n"
+    chmod +x ${PATH_SCRIPT}/cell/v2-b.linux64.sh
+    source ${PATH_SCRIPT}/cell/v2-b.linux64.sh --ver
+}
 
 case $APP_MOD in
   api)
